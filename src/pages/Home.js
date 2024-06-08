4f98928b0fd448e84cd5d51f70b288bddca52e92
Md-JahidHasan <md.jahidhasan.seu@gmail.com>
2024-06-08 23:34:49 +0600

    Component Making

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './../components/navbar/Navbar'
+import Tags from './../components/tags/Tags'
+
+const Home = () => {
+    return (
+        <>
+            <Navbar></Navbar>
+            <Tags></Tags>
+            
+        </>
+    );
+};
+
+export default Home;
