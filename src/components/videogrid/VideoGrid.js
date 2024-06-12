c3b509d572cecbdbd9eca6f530253b72e21a6001
Md-JahidHasan <md.jahidhasan.seu@gmail.com>
2024-06-12 22:08:10 +0600

    Video feature add

--- src/components/videogrid/VideoGrid.js
+++ src/components/videogrid/VideoGrid.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import VideoGridItem from './VideoGridItem';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchVideos } from '../../features/videos/videoSlice';
+import Loading from '../ui/Loading';
 
 const VideoGrid = () => {
+    const dispatch = useDispatch();
+    const { videos, isLoading, isError, error} = useSelector((state) => state.videos)
+
+    console.log(videos);
+
+    useEffect(()=>{
+        dispatch(fetchVideos())
+    }, [dispatch])
+    
+
+
+    // Decide what to render
+    let content;
+
+    if (isLoading) {
+        content = <Loading></Loading>
+    }
+    if (!isLoading && isError) {
+        content = <div className="col-span-12">some error happened</div>
+    }
+    if (!isLoading && !isError && videos?.length > 0) {
+        content = videos.map(video=><VideoGridItem key={video.id} video={video}></VideoGridItem>)
+    }
+
     return (
         <section className="pt-12">
             <section className="pt-12">
@@ -9,8 +36,9 @@ const VideoGrid = () => {
                     className="grid grid-cols-12 gap-4 max-w-7xl mx-auto px-5 lg:px-0 min-h-[300px]"
                 >
                     {/* <!-- single video --> */}
-                    <VideoGridItem></VideoGridItem>
-
+                    
+                    {content}
+                    
                     {/* <!-- error section
                     <div className="col-span-12">some error happened</div> --> */}
                 </div>
